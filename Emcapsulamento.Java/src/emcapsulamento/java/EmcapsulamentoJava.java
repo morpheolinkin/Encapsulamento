0186cb9221f9596582a452ee91eecac926723a9d
Jefferson Medeiros da Silva <30291378+morpheolinkin@users.noreply.github.com>
2018-05-30 15:15:42 -0300

    Add files via upload
    Projeto básico para estudo sobre encapsulamento

---
+++ Emcapsulamento.Java/src/emcapsulamento/java/EmcapsulamentoJava.java
@@ -0,0 +1,12 @@
+package emcapsulamento.java;
+public class EmcapsulamentoJava {
+    public static void main(String[] args) {
+        //Código principal
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.abrirMenu();
+        
+    }
+    
+}
